1c35d75db4daeac15bc8ccc3d27cbc1101756fa4
Cesarinh <160445469+Cesarinh@users.noreply.github.com>
2024-05-06 11:23:02 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,198 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Productos musicales',
+      home: MyHomePage(title: 'iCescMusic'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: PageView(
+        children: [
+          //primera pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/blu.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            "iCescMUsic",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.arrow_circle_right),
+                          Text('Disfruta de la musica'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          //Segunda pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/peso.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Conciertos',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.drive_eta),
+                          Text(
+                            'Encuentra el concierto de tu artista favorito',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          //Tercera pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/tumo.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Festivales',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Text(
+                            'Disfruta de los mejores festivales',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          //Cuarta pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/verde.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Productos',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Text(
+                            'todo el equipo que necesitas',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
